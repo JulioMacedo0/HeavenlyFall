03ef127d710e7ce95344d68f277267abfdfa8b5b
Julio Macedo <juliocesar1076@gmail.com>
2021-02-10 15:46:34 -0300

    fix  Request(temporary soluction)

--- Animacao.js
+++ Animacao.js
@@ -1,3 +1,19 @@
+async function SavePoints() {
+    let body = {
+      user: usuario.name,
+      points: personagem.pontos
+    }
+    console.log(body)
+   await fetch ('http://localhost:3000/user/save', {
+        method: 'POST',
+        body:  JSON.stringify(body),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    }).then((res) => res.json())
+    .then((response) => console.log(response))
+} 
+
 function Animacao(context) {
     this.context = context
     this.HistoryMode = true
